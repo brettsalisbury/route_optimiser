c34b84cdde9382a11114444c76ccd7168809928b
Brett Salisbury <brett.salisbury@gmail.com>
2011-07-14 20:25:27 +1000

    Setup base repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RouteOptimiser::Application.config.secret_token = '4b5e6ce4596e0fa7d88c2772255cab2ac10a921a19b080529ee68489e94e6bb6e1242535e7cf1a10463388e56ead23f9ea6c7f54810efc24fe81c77782a2c7a2'
