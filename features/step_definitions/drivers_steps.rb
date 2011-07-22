3cdecbfcf117bee26dfb70366e0ae45e70529709
Brett Salisbury <brett.salisbury@gmail.com>
2011-07-22 22:20:23 +1000

    Added new feature definition for the driver entry feature

---
+++ features/step_definitions/drivers_steps.rb
@@ -0,0 +1,39 @@
+Given /I have navigated to the drivers page$/ do
+  pending
+end
+
+Given /I enter the drivers name, address and number of passengers they can carry$/ do
+  pending
+end
+
+Given /I press (.*)$/ do | button_id |
+  pending
+end
+
+Given /the driver information will be captured and displayed on the screen$/ do
+  pending
+end
+
+Given /I have previously entered a driver$/ do
+  pending
+end
+
+Given /the driver information is removed$/ do
+  pending
+end
+
+Given /I have entered at least one driver$/ do
+  pending
+end
+
+Given /I will move to the passenger details screen$/ do
+  pending
+end
+
+Given /I have entered no drivers on the driver entry screen$/ do
+  pending
+end
+
+Given /I will return to the driver entry page with an error due to no driver details entered$/ do
+  pending
+end
---
+++ features/step_definitions/home_steps.rb
@@ -0,0 +1,27 @@
+When /I have a (.*) application$/ do |application_name|
+  @app_name = application_name
+end
+
+When /I visit the home page/ do
+  visit root_path
+end
+
+When /I should see the home page/ do
+  page.should have_selector("title", :content => @app_name)
+end
+
+When /I should see some information describing what the site does and how I use it$/ do
+  page.should have_selector("section", :class => "description")
+end
+
+When /I should not see a 404 page/ do
+  page.should_not have_content("404")
+end
+
+When /I want to start the analysis process$/ do
+  click_link("Start")
+end
+
+When /I should see the driver entry page$/ do
+  page.should have_content("Driver Entry")
+end
